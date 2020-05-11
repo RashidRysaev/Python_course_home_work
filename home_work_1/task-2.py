67a46e86b877c85b5e544aa4eebd3dffa7932809
RashidRysaev <rashid.rysaev93@gmail.com>
2020-05-11 19:30:38 +0200

    home_work_1_tasks_added

---
+++ home_work_1/task-2.py
@@ -0,0 +1,15 @@
+# Пользователь вводит время в секундах.
+# Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
+# Используйте форматирование строк.
+
+time_user = int(input("Введите время в секундах: "))
+time_in_hours = time_user // 3600
+remainder = time_user % 3600
+time_in_minutes = remainder // 60
+time_in_seconds = remainder % 60
+
+print(f"Результат равен: {time_in_hours}:{time_in_minutes}:{time_in_seconds}")
+
+
+
+
